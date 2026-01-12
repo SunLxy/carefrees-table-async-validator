22679cb0eac0d6769c4a9750bc2d4557f2f30ddb
SunLxy <1011771396@qq.com>
2026-01-12 11:59:29 +0800

    fix: 修复案例打包加载路径

--- website/rsbuild.config.ts
+++ website/rsbuild.config.ts
@@ -3,5 +3,11 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
+  output: {
+    assetPrefix: '/carefrees-table-async-validator/',
+  },
+  server: {
+    base: '/carefrees-table-async-validator/',
+  },
   plugins: [pluginReact()],
 });
